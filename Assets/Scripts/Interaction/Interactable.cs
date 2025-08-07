a185b034dc4fa5db1481253b49677a756e749440
demirrana <demir.rana.bo@gmail.com>
2025-08-07 14:14:07 +0300

    Interactable.cs simplified
    script made compatible with any kind of interactable

--- Assets/Scripts/Interaction/Interactable.cs
+++ Assets/Scripts/Interaction/Interactable.cs
@@ -5,23 +5,6 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     public event EventHandler OnGotInteracted;
-    
-    private Animator animator;
-
-    private bool toggledOn = false;
-    private const String IsGoing = "IsGoing";
-    private const String IsComing = "IsComing";
-
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();   
-    }
-
-    private void Start()
-    {
-        //Might be a problem since PlayerInteractionManager is disabled and enabled along the game!!!!!!!!
-        OnGotInteracted += GetInteracted_Interactable;
-    }
 
     public void GetInteracted()
     {
@@ -31,22 +14,5 @@ public class Interactable : MonoBehaviour
     public bool CanObjectBeInteracted()
     {
         return true;
-    }
-
-    protected virtual void GetInteracted_Interactable(object sender, EventArgs e)
-    {
-        PlayerScriptsManager.Instance.DisableInteractionScript();
-        Debug.Log("This object is interacted by the player and the interaction script is disabled.");
-
-        if (!toggledOn)
-        {
-            animator.SetTrigger(IsGoing);
-        }
-        else
-        {
-            animator.SetTrigger(IsComing);
-        }
-
-        toggledOn = !toggledOn;
-    }
+    }    
 }
