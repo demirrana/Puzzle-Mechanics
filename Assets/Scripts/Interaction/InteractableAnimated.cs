1ad78202fef6b4dfde75b15c1de2e97db6da9674
demirrana <demir.rana.bo@gmail.com>
2025-08-07 14:15:00 +0300

    InteractableAnimated.cs created
    script for the interactables with animations

---
+++ Assets/Scripts/Interaction/InteractableAnimated.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public class InteractableAnimated : Interactable
+{
+    private Animator animator;
+
+    private bool toggledOn = false;
+    private const String IsGoing = "IsGoing";
+    private const String IsComing = "IsComing";
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        //Might be a problem since PlayerInteractionManager is disabled and enabled along the game!!!!!!!!
+        OnGotInteracted += GetInteracted_Interactable;
+    }
+    
+    protected virtual void GetInteracted_Interactable(object sender, EventArgs e)
+    {
+        PlayerScriptsManager.Instance.DisableInteractionScript();
+        Debug.Log("This object is interacted by the player and the interaction script is disabled.");
+
+        if (!toggledOn)
+        {
+            animator.SetTrigger(IsGoing);
+        }
+        else
+        {
+            animator.SetTrigger(IsComing);
+        }
+
+        toggledOn = !toggledOn;
+    }
+}
