2d29bca473fe3f28778bbd8b9098044a3f27f2d4
demirrana <demir.rana.bo@gmail.com>
2025-08-07 14:43:13 +0300

    Update Interactable.cs
    script generalized over any kind of interactable with GetInteracted method included

--- Assets/Scripts/Interaction/Interactable.cs
+++ Assets/Scripts/Interaction/Interactable.cs
@@ -6,6 +6,13 @@ public class Interactable : MonoBehaviour
 {
     public event EventHandler OnGotInteracted;
 
+    
+    private void Start()
+    {
+        //Might be a problem since PlayerInteractionManager is disabled and enabled along the game!!!!!!!!
+        OnGotInteracted += GetInteracted_Interactable;
+    }
+
     public void GetInteracted()
     {
         OnGotInteracted?.Invoke(this, null);
@@ -14,5 +21,10 @@ public class Interactable : MonoBehaviour
     public bool CanObjectBeInteracted()
     {
         return true;
-    }    
+    }
+    
+    protected virtual void GetInteracted_Interactable(object sender, EventArgs e)
+    {
+        Debug.Log("This object is interacted by the player.");
+    }
 }
