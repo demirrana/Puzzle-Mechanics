7ab5ca3a49ead951403fa57a4382127d7c3b36fb
demirrana <demir.rana.bo@gmail.com>
2025-08-07 17:02:07 +0300

    Update Player5thPuzzleInteractionManager.cs

--- Assets/Scripts/Interaction/Interactable5thPuzzle.cs
+++ Assets/Scripts/Interaction/Interactable5thPuzzle.cs
@@ -1,6 +1,14 @@
+using System;
 using UnityEngine;
 
 public class Interactable5thPuzzle : Interactable
 {
-    
+    private void Start()
+    {
+    }
+
+    override protected void GetInteracted_Interactable(object sender, EventArgs e)
+    {
+        
+    }
 }
