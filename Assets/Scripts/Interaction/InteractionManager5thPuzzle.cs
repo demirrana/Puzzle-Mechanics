7038ca6efb4141ce7659a88a43da4cc76f149050
demirrana <demir.rana.bo@gmail.com>
2025-08-12 00:01:47 +0300

    Update InteractionManager5thPuzzle.cs
    Transferred methods from base class' previous version

--- Assets/Scripts/Interaction/InteractionManager5thPuzzle.cs
+++ Assets/Scripts/Interaction/InteractionManager5thPuzzle.cs
@@ -79,6 +79,102 @@ public class InteractionManager5thPuzzle : InteractionManager
         RaiseInteractionConditionsMet(this, Interactable5thPuzzleTable.Instance); //closing table view
     }
 
+    //5 rays are cast along the height of the player to detect more than one objects near if there are any
+    protected void DetectAnyColliderApproached()
+    {
+        Vector3 rayOriginBottom = transform.position;
+        Vector3 rayOriginTop = transform.position + new Vector3(0f, playerHeight, 0f);
+        Vector3 direction = transform.forward; //I might have to use parent's forward vector
+
+        List<Vector3> rayOrigins = new();
+
+        float differenceInY = (rayOriginTop.y - rayOriginBottom.y) / (rayCount - 1);
+
+        for (int i = 0; i < rayCount; i++)
+        {
+            float currentY = rayOriginBottom.y + i * differenceInY;
+            rayOrigins.Add(new Vector3(rayOriginBottom.x, currentY, rayOriginBottom.z));
+        }
+
+        List<Collider> hitColliders = new List<Collider>();
+
+        foreach (Vector3 rayOrigin in rayOrigins)
+        {
+            if (Physics.Raycast(rayOrigin, direction, out RaycastHit hitInfo, proximityThreshold))
+            {
+                if (!hitColliders.Contains(hitInfo.collider))
+                {
+                    hitColliders.Add(hitInfo.collider);
+                }
+            }
+        }
+
+        switch (hitColliders.Count)
+        {
+            case 0:
+                OnNoInteractableNear?.Invoke(this, null);
+                break;
+            default:
+                OnObjectCollidersApproached?.Invoke(this, hitColliders);
+                break;
+        }
+    }
+
+    protected void ObjectCollidersApproached_PlayerInteractionManager(object sender, List<Collider> colliderList)
+    {
+        List<Interactable> interactableObjects = GetNearInteractablesList(colliderList);
+        DetectInteractableApproached(interactableObjects);
+    }
+
+    protected virtual List<Interactable> GetNearInteractablesList(List<Collider> colliderList)
+    {
+        List<Interactable> interactableObjects = new();
+
+        foreach (Collider collider in colliderList)
+        {
+            if (collider != null)
+            {
+                GameObject hitObject = collider.gameObject;
+
+                //Making sure the object is an interactable one
+                if (hitObject.TryGetComponent<Interactable>(out var interactableObject))
+                {
+                    interactableObjects.Add(interactableObject);
+                }
+            }
+        }
+
+        return interactableObjects;
+    }
+
+    //Gets the Interactable object near and invokes the event OnInteractableApproached with it
+    protected virtual void DetectInteractableApproached(List<Interactable> interactableObjects)
+    {
+        switch (interactableObjects.Count)
+        {
+            case 0:
+                break;
+            case 1:
+                OnInteractableApproached?.Invoke(this, interactableObjects[0]);
+                break;
+            //TO BE CHANGED IN THE FUTURE
+            default:
+                Debug.Log("There are more than 1 interactables");
+                //TO BE CHANGED: When there are more than 1 interactables, the camera angle should decide which one to interact with
+                OnInteractableApproached?.Invoke(this, interactableObjects[1]); //For now, the one after first interactable can be interacted 
+                break;
+        }
+    }
+
+
+    protected void InteractableApproached_PlayerInteractionManager(object sender, Interactable interactable)
+    {
+        foreach (IInteractionBehaviour interactionBehaviour in interactable.GetInteractionBehaviours())
+        {
+            DetectBehaviourApplied(interactable, interactionBehaviour);
+        }
+    }
+
     }
 
     private void SetInstance()
