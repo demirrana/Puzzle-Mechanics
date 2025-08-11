28d1b992165583012eccd408379d7f2cb6a4431f
demirrana <demir.rana.bo@gmail.com>
2025-08-11 16:17:03 +0300

    Update InteractionManager.cs
    Methods and lines deleted to be transferred to child classes

--- Assets/Scripts/Interaction/InteractionManager.cs
+++ Assets/Scripts/Interaction/InteractionManager.cs
@@ -35,75 +35,17 @@ public class InteractionManager : MonoBehaviour
         SetInstance();
     }
 
-    private void Start()
     {
-        OnObjectCollidersApproached += ObjectCollidersApproached_PlayerInteractionManager;
-        OnInteractableApproached += InteractableApproached_PlayerInteractionManager;
     }
 
     {
-            DetectInteractableDropped();
-        DetectAnObjectColliderApproached();
-    }
 
-    //5 rays are cast along the height of the player to detect more than one objects near if there are any
-    protected void DetectAnObjectColliderApproached()
-    {
-        Vector3 rayOriginBottom = transform.position;
-        Vector3 rayOriginTop = transform.position + new Vector3(0f, playerHeight, 0f);
-        Vector3 direction = transform.forward; //I might have to use parent's forward vector
-        
-        List<Vector3> rayOrigins = new();
-
-        float differenceInY = (rayOriginTop.y - rayOriginBottom.y) / (rayCount - 1);
-
-        for (int i = 0; i < rayCount; i++)
-        {
-            float currentY = rayOriginBottom.y + i * differenceInY;
-            rayOrigins.Add(new Vector3(rayOriginBottom.x, currentY, rayOriginBottom.z));
-        }
-
-        List<Collider> hitColliders = new List<Collider>();  
-
-        {
-            if (Physics.Raycast(rayOrigin, direction, out RaycastHit hitInfo, proximityThreshold))
-            {
-                if (!hitColliders.Contains(hitInfo.collider))
-                {
-                    hitColliders.Add(hitInfo.collider);
-                }
-            }
-        }
-
-        switch (hitColliders.Count)
-        {
-            case 0:
-                OnNoInteractableNear?.Invoke(this, null);
-                break;
-            default:
-                OnObjectCollidersApproached?.Invoke(this, hitColliders);
-                break;
-        }
     }
 
     {
     }
 
     {
-
-        foreach (Collider collider in colliderList)
-        {
-            if (collider != null)
-            {
-                GameObject hitObject = collider.gameObject;
-
-                //Making sure the object is an interactable one
-                if (hitObject.TryGetComponent<Interactable>(out var interactableObject))
-                {
-                    interactableObjects.Add(interactableObject);
-                }
-            }
-        }
 
     }
 
