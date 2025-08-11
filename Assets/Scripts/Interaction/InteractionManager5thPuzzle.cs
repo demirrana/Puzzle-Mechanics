8b56ea6e266d3a53ea606aeda9777a9da0e438cd
demirrana <demir.rana.bo@gmail.com>
2025-08-11 23:53:53 +0300

    Update InteractionManager5thPuzzle.cs
    Specific events transferred from base class' previous version

--- Assets/Scripts/Interaction/InteractionManager5thPuzzle.cs
+++ Assets/Scripts/Interaction/InteractionManager5thPuzzle.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class InteractionManager5thPuzzle : InteractionManager
 {
+    public event EventHandler<List<Collider>> OnObjectCollidersApproached;
+    public event EventHandler OnNoInteractableNear;
+    public event EventHandler<Interactable> OnInteractableApproached;
     {
     }
 
