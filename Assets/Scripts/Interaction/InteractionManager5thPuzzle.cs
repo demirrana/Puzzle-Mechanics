bf8d502dee59ad3382c1818976945d7305219f89
demirrana <demir.rana.bo@gmail.com>
2025-08-11 23:54:32 +0300

    Update InteractionManager5thPuzzle.cs

--- Assets/Scripts/Interaction/InteractionManager5thPuzzle.cs
+++ Assets/Scripts/Interaction/InteractionManager5thPuzzle.cs
@@ -1,12 +1,23 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractionManager5thPuzzle : InteractionManager
 {
+    public static InteractionManager5thPuzzle Instance { get; private set; }
+
     public event EventHandler<List<Collider>> OnObjectCollidersApproached;
     public event EventHandler OnNoInteractableNear;
     public event EventHandler<Interactable> OnInteractableApproached;
     {
     }
 
+    private void SetInstance()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
     }
+}
