d330a0651a4cc7b08fb0839d0f2f5b073399baa2
demirrana <demir.rana.bo@gmail.com>
2025-08-12 09:39:05 +0300

    Update InteractionManager.cs
    The method for getting near colliders is transferred from InteractionManager5thPuzzle to here since other subclasses may use this

--- Assets/Scripts/Interaction/InteractionManager.cs
+++ Assets/Scripts/Interaction/InteractionManager.cs
@@ -83,6 +83,39 @@ public class InteractionManager : MonoBehaviour
         }
     }
 
+    //5 rays are cast along the height of the player to detect more than one objects near if there are any
+    protected List<Collider> GetCollidersApproached()
+    {
+        Vector3 rayOriginBottom = transform.position;
+        Vector3 rayOriginTop = transform.position + new Vector3(0f, playerHeight, 0f);
+        Vector3 direction = transform.forward; //I might have to use parent's forward vector
+
+        List<Vector3> rayOrigins = new();
+
+        float differenceInY = (rayOriginTop.y - rayOriginBottom.y) / (rayCount - 1);
+
+        for (int i = 0; i < rayCount; i++)
+        {
+            float currentY = rayOriginBottom.y + i * differenceInY;
+            rayOrigins.Add(new Vector3(rayOriginBottom.x, currentY, rayOriginBottom.z));
+        }
+
+        List<Collider> hitColliders = new List<Collider>();
+
+        foreach (Vector3 rayOrigin in rayOrigins)
+        {
+            if (Physics.Raycast(rayOrigin, direction, out RaycastHit hitInfo, proximityThreshold))
+            {
+                if (!hitColliders.Contains(hitInfo.collider))
+                {
+                    hitColliders.Add(hitInfo.collider);
+                }
+            }
+        }
+
+        return hitColliders;
+    }
+
     protected void RaiseInteractionConditionsMet(object sender, Interactable interactable)
     {
         OnInteractionConditionsMet?.Invoke(sender, interactable);
