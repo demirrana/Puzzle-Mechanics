ad02e1810195071155afe4ad88f7fa020284bfda
demirrana <demir.rana.bo@gmail.com>
2025-09-03 23:55:29 +0300

    Update InteractionManager7thPuzzle.cs
    EventHandlers are added to control the approaching interactables flow

--- Assets/Scripts/Interaction/InteractionManager7thPuzzle.cs
+++ Assets/Scripts/Interaction/InteractionManager7thPuzzle.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
     public static InteractionManager7thPuzzle Instance { get; private set; }
+
+    public event EventHandler<List<Collider>> OnObjectCollidersApproached;
+    public event EventHandler OnNoInteractableNear;
+    public event EventHandler<Interactable7thPuzzleObject> OnInteractableApproached;
     private void Awake()
     {
         SetInstance();
