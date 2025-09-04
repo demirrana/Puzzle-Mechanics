cad879e2711d4002546b7a8c2531324aad144990
demirrana <demir.rana.bo@gmail.com>
2025-09-04 15:29:44 +0300

    Update InteractionManager7thPuzzle.cs
    Interactable that is approached at that moment is stored

--- Assets/Scripts/Interaction/InteractionManager7thPuzzle.cs
+++ Assets/Scripts/Interaction/InteractionManager7thPuzzle.cs
@@ -14,6 +14,8 @@ using UnityEngine;
     private InteractionPanelIndividual InteractionPanelIndividual;
     private Interactable7thPuzzleObject interactable1stChosen;
     private Interactable7thPuzzleObject interactable2ndChosen; //2nd interactable chosen to swap with the one in hand
+    private Interactable7thPuzzleObject approachedInteractable;
+
     private void Awake()
     {
         SetInstance();
