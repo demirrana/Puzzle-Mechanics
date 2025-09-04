32c5a183853ec72b795d01c69a594b5e1ecba842
demirrana <demir.rana.bo@gmail.com>
2025-09-04 15:27:50 +0300

    Update InteractionManager7thPuzzle.cs
    VFX for choosing an interactable

--- Assets/Scripts/Interaction/InteractionManager7thPuzzle.cs
+++ Assets/Scripts/Interaction/InteractionManager7thPuzzle.cs
@@ -7,6 +7,10 @@ using UnityEngine;
     public event EventHandler<List<Collider>> OnObjectCollidersApproached;
     public event EventHandler OnNoInteractableNear;
     public event EventHandler<Interactable7thPuzzleObject> OnInteractableApproached;
+
+    [SerializeField] private ParticleSystem choosingCircle1;
+    [SerializeField] private ParticleSystem choosingCircle2;
+
     private void Awake()
     {
         SetInstance();
