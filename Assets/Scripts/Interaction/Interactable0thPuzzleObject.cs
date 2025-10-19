56b787d228e8c18e64fd657969522d4238963a90
demirrana <demir.rana.bo@gmail.com>
2025-10-19 15:42:59 +0300

    Add method to update book state after behaviour
    -Implements UpdateState method
    -When a behaviour is applied, Book's parent and position are updated with the given new ones

--- Assets/Scripts/Interaction/Interactable0thPuzzleObject.cs
+++ Assets/Scripts/Interaction/Interactable0thPuzzleObject.cs
@@ -24,4 +24,11 @@ public class Interactable0thPuzzleObject : Interactable0thPuzzle
         UpdateBehavioursAfter<IInteractableBehaviour0thPuzzle>(interactionBehaviour);
     }
 
+    public void UpdateState(Vector3 newPosition, Transform newParent)
+    {
+        transform.parent = null; //makes parent null before updating position
+        transform.position = newPosition;
+        transform.parent = newParent;
+        //OnInteractableStateChanged?.Invoke(this, newState);
+    }
 }
