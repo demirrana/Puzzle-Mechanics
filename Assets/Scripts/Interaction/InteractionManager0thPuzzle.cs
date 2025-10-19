8f72c19d7bc7cba515c13a43ae3fac9f7423361c
demirrana <demir.rana.bo@gmail.com>
2025-10-19 14:33:04 +0300

    Add singleton instance property to InteractionManager0thPuzzle

--- Assets/Scripts/Interaction/InteractionManager0thPuzzle.cs
+++ Assets/Scripts/Interaction/InteractionManager0thPuzzle.cs
@@ -4,4 +4,6 @@ using UnityEngine;
 
 public class InteractionManager0thPuzzle : InteractionManager<IInteractableBehaviour0thPuzzle> //to be changed to 0thPuzzle
 {
+    public static InteractionManager0thPuzzle Instance { get; private set; }
+
 }
