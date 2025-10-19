09ce605f780041bc1dd7e5f381ef2892e8f34da2
demirrana <demir.rana.bo@gmail.com>
2025-10-19 15:26:51 +0300

    Add HasPlace method to check if platform has space for books
    -Returns true when the total number of books on the platform is not 8 yet
    -Will be used as a condition to trigger events in InteractionManager0thPuzzle.cs

--- Assets/Scripts/Interaction/Interactable0thPuzzlePlatform.cs
+++ Assets/Scripts/Interaction/Interactable0thPuzzlePlatform.cs
@@ -9,6 +9,11 @@ public class Interactable0thPuzzlePlatform : Interactable0thPuzzle
 
     private readonly int BookCapacity = 8;
 
+    public bool HasPlace()
+    {
+        return booksOnPlatform.Count < BookCapacity;
+    }
+
     private void SetInstance()
     {
         if (Instance != null && Instance != this)
