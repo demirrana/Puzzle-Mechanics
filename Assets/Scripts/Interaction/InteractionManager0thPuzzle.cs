c848a3f8608b8f9a0975fbd3b5aab0775a9db5c9
demirrana <demir.rana.bo@gmail.com>
2025-10-19 12:32:16 +0300

    InteractionManager0thPuzzle.cs created

---
+++ Assets/Scripts/Interaction/InteractionManager0thPuzzle.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager0thPuzzle : InteractionManager<IInteractableBehaviour0thPuzzle> //to be changed to 0thPuzzle
+{
+}
