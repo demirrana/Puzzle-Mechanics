511006f1102fa6ba46dafabad42c2d0701b6a98b
demirrana <demir.rana.bo@gmail.com>
2025-10-19 15:05:06 +0300

    Add SetInstance method to manage singleton lifecycle

--- Assets/Scripts/Interaction/Interactable0thPuzzlePlatform.cs
+++ Assets/Scripts/Interaction/Interactable0thPuzzlePlatform.cs
@@ -4,4 +4,12 @@ using UnityEngine;
 public class Interactable0thPuzzlePlatform : Interactable0thPuzzle
 {
     public static Interactable0thPuzzlePlatform Instance { get; private set; }
+    private void SetInstance()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
 }
