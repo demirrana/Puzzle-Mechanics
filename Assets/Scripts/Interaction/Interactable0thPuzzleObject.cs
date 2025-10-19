44c0d63e022a78856fccfcb9e0b61774fb803733
demirrana <demir.rana.bo@gmail.com>
2025-10-19 11:16:37 +0300

    Update Interactable0thPuzzleObject.cs
    Behaviours list contains only the behaviour of picking up from the shelf since all books are in the shelf at Awake

--- Assets/Scripts/Interaction/Interactable0thPuzzleObject.cs
+++ Assets/Scripts/Interaction/Interactable0thPuzzleObject.cs
@@ -4,6 +4,12 @@ public class Interactable0thPuzzleObject : Interactable0thPuzzle
 {
     public SOBookData bookData;
     [SerializeField] private Vector3 bookPlaceOnShelf;
+
+    private void Awake()
+    {
+        behavioursList.Add(new InteractableBehaviourPickUpFromShelf());
+    }
+
     public Vector3 GetBookPlaceOnShelf()
     {
         return bookPlaceOnShelf;
