5db12ce0e0b5a047db7d4975d006ec697280e310
demirrana <demir.rana.bo@gmail.com>
2025-10-19 11:18:45 +0300

    Interactable behaviour classes created for 0th puzzle
    All behaviours are decided and created with their superclass

---
+++ Assets/Scripts/Interaction/IInteractableBehaviour0thPuzzle.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IInteractableBehaviour0thPuzzle : IInteractableBehaviour
+{
+}
+
+public class InteractableBehaviourPickUpFromShelf : IInteractableBehaviour0thPuzzle
+}
+
+public class InteractableBehaviourPutOnShelf : IInteractableBehaviour0thPuzzle
+}
+
+public class InteractableBehaviourPutOnBookPlatform : IInteractableBehaviour0thPuzzle
+{
+}
+
+public class InteractableBehaviourPickUpFromBookPlatform : IInteractableBehaviour0thPuzzle
+{
+}
