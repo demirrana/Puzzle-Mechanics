7f29d45117e9ad4429e99e5fd4ba8bee5b664ed0
demirrana <demir.rana.bo@gmail.com>
2025-10-19 15:16:00 +0300

    Add list to store all books on the book platform

--- Assets/Scripts/Interaction/Interactable0thPuzzlePlatform.cs
+++ Assets/Scripts/Interaction/Interactable0thPuzzlePlatform.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class Interactable0thPuzzlePlatform : Interactable0thPuzzle
 {
     public static Interactable0thPuzzlePlatform Instance { get; private set; }
+
+    private List<Interactable0thPuzzleObject> booksOnPlatform;
+
     private void SetInstance()
     {
         if (Instance != null && Instance != this)
