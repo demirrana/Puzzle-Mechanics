ca7ced569806fad3ed44353741022e1a705d506a
demirrana <demir.rana.bo@gmail.com>
2025-10-19 11:14:55 +0300

    Update Interactable0thPuzzleObject.cs
    Each book has its own place in the shelves and stores it

--- Assets/Scripts/Interaction/Interactable0thPuzzleObject.cs
+++ Assets/Scripts/Interaction/Interactable0thPuzzleObject.cs
@@ -3,4 +3,10 @@ using UnityEngine;
 public class Interactable0thPuzzleObject : Interactable0thPuzzle
 {
     public SOBookData bookData;
+    [SerializeField] private Vector3 bookPlaceOnShelf;
+    public Vector3 GetBookPlaceOnShelf()
+    {
+        return bookPlaceOnShelf;
+    }
+
 }
