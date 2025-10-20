bc065d19e7d14e7f968e789280895d6710f79cf3
demirrana <demir.rana.bo@gmail.com>
2025-10-20 23:16:26 +0300

    feat: manage conditions meeting logic through Update

--- Assets/Scripts/Interaction/InteractionManager0thPuzzle.cs
+++ Assets/Scripts/Interaction/InteractionManager0thPuzzle.cs
@@ -35,6 +35,10 @@ public class InteractionManager0thPuzzle : InteractionManager<IInteractableBehav
         InitializeObjects();
     }
 
+    private void Update()
+    {
+        DetectInteractionConditionsMet();
+    }
 
     public Transform GetObjectsHolderTransform()
     {
