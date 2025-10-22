33c306c3e2543cc8b009fbd9169f1c6772a9b29d
demirrana <demir.rana.bo@gmail.com>
2025-10-22 15:36:49 +0300

    feat: add events to control active view flow
    -Add 2 events for world view and platform view activations

--- Assets/Scripts/Interaction/InteractionManager0thPuzzle.cs
+++ Assets/Scripts/Interaction/InteractionManager0thPuzzle.cs
@@ -7,6 +7,8 @@ public class InteractionManager0thPuzzle : InteractionManager<IInteractableBehav
     public static InteractionManager0thPuzzle Instance { get; private set; }
 
     [SerializeField] private Transform puzzle0ObjectsHolder;
+    public event EventHandler OnWorldViewActivated;
+    public event EventHandler OnPlatformViewActivated;
 
 
     private enum GameState
