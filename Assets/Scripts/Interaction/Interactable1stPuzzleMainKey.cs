4d7be71d83ffdb2518740bb81ba3115939df1cf9
demirrana <demir.rana.bo@gmail.com>
2026-01-03 16:34:39 +0300

    feat: rotate main key inside edit view
    -Include velocity to rotate main key smoothly by adding inertia

--- Assets/Scripts/Interaction/Interactable1stPuzzleMainKey.cs
+++ Assets/Scripts/Interaction/Interactable1stPuzzleMainKey.cs
@@ -4,10 +4,46 @@ public class Interactable1stPuzzleMainKey : MonoBehaviour
 {
     public static Interactable1stPuzzleMainKey Instance { get; private set; }
 
+    public float rotationSpeed = 5f;
+    private Vector3 velocity;
+    private bool isDragging = false;
+
     private void Awake()
     {
         SetInstance();
     }
+
+    public bool IsDragging()
+    {
+        return isDragging;
+    }
+
+    public void RotateObject()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        if (isDragging)
+        {
+            float rotX = Input.GetAxis("Mouse X") * rotationSpeed;
+            float rotY = Input.GetAxis("Mouse Y") * rotationSpeed;
+            velocity = new Vector3(rotX, rotY, 0);
+        }
+        else
+        {
+            velocity = Vector3.Lerp(velocity, Vector3.zero, Time.deltaTime * 5f);
+        }
+
+        transform.Rotate(Vector3.up, -velocity.x, Space.World);
+        transform.Rotate(Vector3.right, velocity.y, Space.World);
+    }
+
     private void SetInstance()
     {
         if (Instance != null && Instance != this)
