5c27a3ac7e456dd5b613415ac4119f30c87cad24
demirrana <demir.rana.bo@gmail.com>
2026-01-04 14:54:09 +0300

    feat: add SO data to each key part
    -Add getter methods for SO data and each element of it (key part ID, display name, icon used to display on inventory slot, prefab of the key part)

--- Assets/Scripts/Interaction/Interactable1stPuzzleObject.cs
+++ Assets/Scripts/Interaction/Interactable1stPuzzleObject.cs
@@ -1,7 +1,11 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class Interactable1stPuzzleObject : Interactable1stPuzzle
 {
+    [SerializeField] private SOCollectibleKeyPart keyPartData;
 
     private void Awake()
     {
@@ -13,4 +17,28 @@ public class Interactable1stPuzzleObject : Interactable1stPuzzle
         transform.parent = parentTransform;
     }
 
+    public SOCollectibleKeyPart GetKeyPartData()
+    {
+        return keyPartData;
+    }
+
+    public String GetKeyPartID()
+    {
+        return keyPartData.keyPartID;
+    }
+
+    public String GetDisplayName()
+    {
+        return keyPartData.displayName;
+    }
+
+    public Sprite GetInventoryIcon()
+    {
+        return keyPartData.inventoryIcon;
+    }
+
+    public GameObject GetPrefab()
+    {
+        return keyPartData.prefab;
+    }
 }
