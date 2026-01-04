6147953cd7749443e4a8f457a70179af23289c9d
demirrana <demir.rana.bo@gmail.com>
2026-01-04 17:41:52 +0300

    feat: add rotating around object function

--- Assets/Scripts/Camera/PreviewCamera.cs
+++ Assets/Scripts/Camera/PreviewCamera.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 public class PreviewCamera : MonoBehaviour
 {
     public static PreviewCamera Instance { get; private set; }
+
+    private float rotationSpeed = 12f;
+
+    public void RotateAroundObject(GameObject gameObject)
+    {
+        transform.RotateAround(gameObject.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+    
     private void Awake()
     {
         SetInstance();
