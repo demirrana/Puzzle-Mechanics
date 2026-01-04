643ae49a841a1af7adbc9f039a0beb5daeba593e
demirrana <demir.rana.bo@gmail.com>
2026-01-04 19:17:14 +0300

    feat: add UI manager class for puzzle 1

---
+++ Assets/Scripts/UI/UI_Puzzle1Manager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UI_Puzzle1Manager : MonoBehaviour
+{
+    public static UI_Puzzle1Manager Instance { get; private set; }
+
+    private void Awake()
+    {
+        SetInstance();
+    }
+
+    private void SetInstance()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
+}
