2d84bd1bca69ea0939b21f2542ccdf83d2096995
demirrana <demir.rana.bo@gmail.com>
2026-01-06 20:25:16 +0300

    feat: create event args to use during change in hovered slot
    -Hold previous and current slot in new event arguments

--- Assets/Scripts/UI/UI_Puzzle1Manager.cs
+++ Assets/Scripts/UI/UI_Puzzle1Manager.cs
@@ -8,6 +8,18 @@ public class UI_Puzzle1Manager : MonoBehaviour
 {
     public static UI_Puzzle1Manager Instance { get; private set; }
 
+    public class ChangeInHoveredObjectEventArgs : EventArgs
+    {
+        public UI_Puzzle1stObject PreviousInventorySlot { get; }
+        public UI_Puzzle1stObject CurrentInventorySlot { get; }
+
+        public ChangeInHoveredObjectEventArgs(UI_Puzzle1stObject previousInventorySlot, UI_Puzzle1stObject currentInventorySlot)
+        {
+            PreviousInventorySlot = previousInventorySlot;
+            CurrentInventorySlot = currentInventorySlot;
+        }
+    }
+
     public event EventHandler OnInventoryPanelActivated;
     public event EventHandler OnInventoryPanelDeactivated;
     private void Awake()
