7e4c1fc91c83197604a8d5c1fee8e6bae5dd70ca
demirrana <demir.rana.bo@gmail.com>
2026-01-06 20:26:29 +0300

    feat: add event to detect when hovered slot doesn't exist

--- Assets/Scripts/UI/UI_Puzzle1Manager.cs
+++ Assets/Scripts/UI/UI_Puzzle1Manager.cs
@@ -23,6 +23,7 @@ public class UI_Puzzle1Manager : MonoBehaviour
     public event EventHandler OnInventoryPanelActivated;
     public event EventHandler OnInventoryPanelDeactivated;
     public event EventHandler<ChangeInHoveredObjectEventArgs> OnHoveredInventorySlotChanged;
+    public event EventHandler OnNoHoveredInventorySlot;
     private void Awake()
     {
         SetInstance();
