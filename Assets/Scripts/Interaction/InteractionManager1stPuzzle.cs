51858f8ff4a93771923b1b236197174a18d05190
demirrana <demir.rana.bo@gmail.com>
2026-01-06 15:52:18 +0300

    import necessary libraries

--- Assets/Scripts/Interaction/InteractionManager1stPuzzle.cs
+++ Assets/Scripts/Interaction/InteractionManager1stPuzzle.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Generic;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class InteractionManager1stPuzzle : InteractionManager<IInteractableBehaviour1stPuzzle> //to be changed into 1stPuzzle
 {
