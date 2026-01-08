b4ea3f934822522074e2e6128067d210bb190cc7
demirrana <demir.rana.bo@gmail.com>
2026-01-08 22:20:06 +0300

    feat: let each key have its sockets and plug

--- Assets/Scripts/Interaction/Interactable1stPuzzleObject.cs
+++ Assets/Scripts/Interaction/Interactable1stPuzzleObject.cs
@@ -1,11 +1,17 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Interactable1stPuzzleObject : Interactable1stPuzzle
 {
+    public List<SocketData> sockets;
+
     [SerializeField] private SOCollectibleKeyPart keyPartData;
+
+    [SerializeField] private Transform plugTransform;
+    [SerializeField] private String plugID;
 
     private void Awake()
     {
@@ -40,5 +46,15 @@ public class Interactable1stPuzzleObject : Interactable1stPuzzle
     public GameObject GetPrefab()
     {
         return keyPartData.prefab;
+    }
+
+    public String GetPlugID()
+    {
+        return plugID;
+    }
+
+    public Transform GetPlugTransform()
+    {
+        return plugTransform;
     }
 }
